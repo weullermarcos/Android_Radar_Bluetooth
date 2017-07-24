30a6a2e5c4956b2d2d23191b4d6008baedf7e8ed
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2017-07-24 14:57:11 -0300

    ajustando layout

--- RadarAndroid/app/src/main/java/com/example/weuller/radarandroid/Models/SemiCirculo.java
+++ RadarAndroid/app/src/main/java/com/example/weuller/radarandroid/Models/SemiCirculo.java
@@ -8,6 +8,7 @@ import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -28,16 +29,17 @@ public class SemiCirculo extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        //pega a lagura da tela
         float width = (float) getWidth();
-        float height = (float) getHeight();
-        float radius;
 
-        if (width > height) {
-            radius = height / 4;
-        }
-        else {
-            radius = width / 4;
-        }
+        //pega a altura da tela
+        float height = (float) getHeight();
+
+        //raio do circulo
+        float radius = (width/2);
+
+        Log.d("width", String.valueOf(width));
+        Log.d("RAIO", String.valueOf(radius));
 
         Path path = new Path();
         path.addCircle(width / 2, height / 2, radius, Path.Direction.CW);
@@ -51,8 +53,11 @@ public class SemiCirculo extends View {
         final RectF oval = new RectF();
         paint.setStyle(Paint.Style.STROKE);
 
+        //posiciona horizontalmente no centro da tela
         center_x = width / 2;
-        center_y = height / 2;
+
+        //posicionar no fundo da tela, um pouco a cima
+        center_y = height;
 
         oval.set(center_x - radius, center_y - radius, center_x + radius, center_y + radius);
         canvas.drawArc(oval, 180, 180, true, paint);
