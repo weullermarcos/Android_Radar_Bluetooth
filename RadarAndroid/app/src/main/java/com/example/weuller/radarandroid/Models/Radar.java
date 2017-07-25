ef4e897fd059220dccacff542687fbf3ede9d1f1
Weuller Marcos <weullermarcos@MBP-de-Weuller.lan>
2017-07-24 22:04:06 -0300

    mais mudancas

--- RadarAndroid/app/src/main/java/com/example/weuller/radarandroid/Models/Radar.java
+++ RadarAndroid/app/src/main/java/com/example/weuller/radarandroid/Models/Radar.java
@@ -16,7 +16,7 @@ import android.view.View;
 
 public class Radar extends View {
 
-    private static final int HORIZONTAL_MARGIN = 100;
+    private static final int HORIZONTAL_MARGIN = 120;
 
     public Radar(Context context){
         this(context, null);
@@ -31,9 +31,10 @@ public class Radar extends View {
         super.onDraw(canvas);
 
         //pega a lagura da tela
-        float width = (float) getWidth() - HORIZONTAL_MARGIN;
-        Log.d("WIDTH", String.valueOf(width));
-        float distanceBetweenPoints = (float) width/8;
+        float width = (float) getWidth();
+        float widthWithMargin = width - HORIZONTAL_MARGIN;
+        Log.d("WIDTH", String.valueOf(widthWithMargin));
+        float distanceBetweenPoints = (float) widthWithMargin/8;
         Log.d("DISTANCE", String.valueOf(distanceBetweenPoints));
 
         //pega a altura da tela
@@ -49,7 +50,7 @@ public class Radar extends View {
 
         float center_x, center_y;
         //posiciona horizontalmente no centro da tela
-        center_x = (float) getWidth() / 2;
+        center_x = width / 2;
         Log.d("center_x", String.valueOf(center_x));
         //posicionar no fundo da tela, um pouco a cima
         center_y = height / 2;
@@ -57,7 +58,7 @@ public class Radar extends View {
 
 
         //raios dos circulos
-        float radius1 = (width/2);
+        float radius1 = (widthWithMargin/2);
         float radius2 = radius1 - distanceBetweenPoints;
         float radius3 = radius2 - distanceBetweenPoints;
         float radius4 = radius3 - distanceBetweenPoints;
@@ -67,22 +68,28 @@ public class Radar extends View {
         Log.d("RAIO3", String.valueOf(radius3));
         Log.d("RAIO4", String.valueOf(radius4));
 
-        RectF oval1 = new RectF();
-//        oval1.set(center_x, center_y , center_x , center_y );
-        oval1.set(center_x - radius1, center_y - radius1, center_x + radius1, center_y + radius1);
-        canvas.drawArc(oval1, 180, 180, true, paint);
+        //arco 1 (MAIOR)
+        RectF oval = new RectF();
+        oval.set(center_x - radius1, center_y - radius1, center_x + radius1, center_y + radius1);
+        canvas.drawArc(oval, 180, 180, true, paint);
 
-        oval1 = new RectF();
-        oval1.set(center_x - radius2, center_y - radius2, center_x + radius2, center_y + radius2);
-        canvas.drawArc(oval1, 180, 180, true, paint);
+        //arco 2
+        oval = new RectF();
+        oval.set(center_x - radius2, center_y - radius2, center_x + radius2, center_y + radius2);
+        canvas.drawArc(oval, 180, 180, true, paint);
 
-        oval1 = new RectF();
-        oval1.set(center_x - radius3, center_y - radius3, center_x + radius3, center_y + radius3);
-        canvas.drawArc(oval1, 180, 180, true, paint);
+        //arco 3
+        oval = new RectF();
+        oval.set(center_x - radius3, center_y - radius3, center_x + radius3, center_y + radius3);
+        canvas.drawArc(oval, 180, 180, true, paint);
 
-        oval1 = new RectF();
-        oval1.set(center_x - radius4, center_y - radius4, center_x + radius4, center_y + radius4);
-        canvas.drawArc(oval1, 180, 180, true, paint);
+        //arco 4 (MENOR)
+        oval = new RectF();
+        oval.set(center_x - radius4, center_y - radius4, center_x + radius4, center_y + radius4);
+        canvas.drawArc(oval, 180, 180, true, paint);
+
+        //LINHA INFERIOR
+        canvas.drawLine(center_x, center_y, width, center_y, paint);
 
     }
 
