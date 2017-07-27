efedc45c2be9d3b3ac98c8dfdb668f9277be3a7d
Weuller Marcos da Silva Santos <weuller@redecfa.org.br>
2017-07-27 11:23:28 -0300

    desenhando distancias e angulos

--- RadarAndroid/app/src/main/java/com/example/weuller/radarandroid/Models/Radar.java
+++ RadarAndroid/app/src/main/java/com/example/weuller/radarandroid/Models/Radar.java
@@ -19,6 +19,16 @@ public class Radar extends View {
     private static final int HORIZONTAL_MARGIN = 120;
     private static final int INITIAL_ANGLE = 15;
     private static final int FINAL_ANGLE = 165;
+    private static final int X_BACK_TEXT_CONSTANT = 25;
+    private static final int Y_BACK_TEXT_CONSTANT = 5;
+    private static final int DELAY = 30;
+
+    private static final int ANGLE_30 = 30;
+    private static final int ANGLE_60 = 60;
+    private static final int ANGLE_90 = 90;
+    private static final int ANGLE_120 = 120;
+    private static final int ANGLE_150 = 150;
+    private static final int ANGLE_180 = 180;
 
     private float startX = 0;
     private float startY = 0;
@@ -49,7 +59,6 @@ public class Radar extends View {
         float height = (float) getHeight();
         Log.d("HEIGHT", String.valueOf(height));
 
-
         float center_x, center_y;
         //posiciona horizontalmente no centro da tela
         center_x = width / 2;
@@ -59,7 +68,6 @@ public class Radar extends View {
         center_y = height / 2;
         startY = center_y;
         Log.d("center_y", String.valueOf(center_y));
-
 
         //raios dos circulos
         float radius1 = (widthWithMargin/2);
@@ -75,48 +83,82 @@ public class Radar extends View {
         //arco 1 (MAIOR)
         RectF oval = new RectF();
         oval.set(center_x - radius1, center_y - radius1, center_x + radius1, center_y + radius1);
-        canvas.drawArc(oval, 180, 180, true, paint);
+        canvas.drawArc(oval, ANGLE_180, ANGLE_180, true, paint);
 
         //arco 2
         oval = new RectF();
         oval.set(center_x - radius2, center_y - radius2, center_x + radius2, center_y + radius2);
-        canvas.drawArc(oval, 180, 180, true, paint);
+        canvas.drawArc(oval, ANGLE_180, ANGLE_180, true, paint);
 
         //arco 3
         oval = new RectF();
         oval.set(center_x - radius3, center_y - radius3, center_x + radius3, center_y + radius3);
-        canvas.drawArc(oval, 180, 180, true, paint);
+        canvas.drawArc(oval, ANGLE_180, ANGLE_180, true, paint);
 
         //arco 4 (MENOR)
         oval = new RectF();
         oval.set(center_x - radius4, center_y - radius4, center_x + radius4, center_y + radius4);
-        canvas.drawArc(oval, 180, 180, true, paint);
+        canvas.drawArc(oval, ANGLE_180, ANGLE_180, true, paint);
 
         //LINHA 0 graus
         canvas.drawLine(center_x, center_y, width, center_y, paint);
 
         //LINHA 30 graus
-        float xFinal = center_x + catetoAdjacente(30, radius1);
-        float yFinal = center_y - catetoOposto(30, radius1);
+        float xFinal = center_x + catetoAdjacente(ANGLE_30, radius1);
+        float yFinal = center_y - catetoOposto(ANGLE_30, radius1);
         canvas.drawLine(center_x, center_y, xFinal, yFinal, paint);
+
+        // desenhando o texto de 30°
+        canvas.save();
+        canvas.rotate(65, (xFinal + 7), (yFinal - 17));
+        canvas.drawText("30°", (xFinal + 7), (yFinal - 17), paintText);
+        canvas.restore();
 
         //LINHA 60 graus
-        xFinal = center_x + catetoAdjacente(60, radius1);
-        yFinal = center_y - catetoOposto(60, radius1);
+        xFinal = center_x + catetoAdjacente(ANGLE_60, radius1);
+        yFinal = center_y - catetoOposto(ANGLE_60, radius1);
         canvas.drawLine(center_x, center_y, xFinal, yFinal, paint);
+
+        // desenhando o texto de 60°
+        canvas.save();
+        canvas.rotate(35, (xFinal + 2), (yFinal - 15));
+        canvas.drawText("60°", (xFinal + 2), (yFinal - 15), paintText);
+        canvas.restore();
 
         //LINHA 90 graus
-        canvas.drawLine(center_x, center_y, center_x, (center_y - radius1 - 20), paint);
+        canvas.drawLine(center_x, center_y, center_x, (center_y - radius1), paint);
+
+        // desenhando o texto de 90°
+        canvas.drawText("90°", (center_x - 10), (center_y - radius1 - 10), paintText);
 
         //LINHA 120 graus
-        xFinal = center_x + catetoAdjacente(120, radius1);
-        yFinal = center_y - catetoOposto(120, radius1);
+        xFinal = center_x + catetoAdjacente(ANGLE_120, radius1);
+        yFinal = center_y - catetoOposto(ANGLE_120, radius1);
         canvas.drawLine(center_x, center_y, xFinal, yFinal, paint);
 
+        // desenhando o texto de 120°
+        canvas.save();
+        canvas.rotate(-30, (xFinal - 15), (yFinal - 5));
+        canvas.drawText("120°", (xFinal - 15), (yFinal - 5), paintText);
+        canvas.restore();
+
         //LINHA 150 graus
-        xFinal = center_x + catetoAdjacente(150, radius1);
-        yFinal = center_y - catetoOposto(150, radius1);
+        xFinal = center_x + catetoAdjacente(ANGLE_150, radius1);
+        yFinal = center_y - catetoOposto(ANGLE_150, radius1);
         canvas.drawLine(center_x, center_y, xFinal, yFinal, paint);
+
+        // desenhando o texto de 150°
+        canvas.save();
+        canvas.rotate(-55, (xFinal - 10), (yFinal - 5));
+        canvas.drawText("150°", (xFinal - 10), (yFinal - 5), paintText);
+        canvas.restore();
+
+        //desenhando distancias
+        canvas.drawText("40 cm", (center_x + radius1 - X_BACK_TEXT_CONSTANT), (center_y - Y_BACK_TEXT_CONSTANT), paintText);
+        canvas.drawText("30 cm", (center_x + radius2 - X_BACK_TEXT_CONSTANT), (center_y - Y_BACK_TEXT_CONSTANT), paintText);
+        canvas.drawText("20 cm", (center_x + radius3 - X_BACK_TEXT_CONSTANT), (center_y - Y_BACK_TEXT_CONSTANT), paintText);
+        canvas.drawText("10 cm", (center_x + radius4 - X_BACK_TEXT_CONSTANT), (center_y - Y_BACK_TEXT_CONSTANT), paintText);
+
 
         xFinal = center_x + catetoAdjacente(currentAngle, radius1);
         yFinal = center_y - catetoOposto(currentAngle, radius1);
@@ -135,15 +177,25 @@ public class Radar extends View {
             currentAngle --;
         }
 
-        postInvalidateDelayed(30); // set time here
+        postInvalidateDelayed(DELAY); // set time here
     }
 
     private Paint paint = new Paint() {
         {
             setColor(Color.GREEN);
-            setStrokeWidth(5);
-            setStyle(Paint.Style.FILL);
             setStyle(Paint.Style.STROKE);
+            setStrokeWidth(5);
+            setAntiAlias(true);
+        }
+    };
+
+    private Paint paintText = new Paint() {
+        {
+            setColor(Color.GREEN);
+            setStyle(Paint.Style.STROKE);
+            setStrokeWidth(2);
+            setTextSize(20);
+            setAntiAlias(true);
         }
     };
 
@@ -153,6 +205,7 @@ public class Radar extends View {
             setStrokeWidth(5);
             setStyle(Paint.Style.FILL);
             setStyle(Paint.Style.STROKE);
+            setAntiAlias(true);
         }
     };
 
